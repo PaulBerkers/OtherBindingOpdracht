0a6f027744fb89f34985ccc0240eb668123943b9
Paul Berkers <ps179791@summacollege.nl>
2020-03-04 19:14:43 +0100

    Add project files.

---
+++ OtherBindingOpdracht/Classes/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtherBindingOpdracht.Classes
+{
+    public sealed partial class Person
+    {
+        private string _title;
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+        private string _auteur;
+
+        public string Auteur
+        {
+            get { return _auteur; }
+            set { _auteur = value; }
+        }
+        private int _aantal;
+
+        public int Aantal
+        {
+            get { return _aantal; }
+            set { _aantal = value; }
+        }
+
+        private string _beschrijving;
+
+        public string Beschrijving
+        {
+            get { return _beschrijving; }
+            set { _beschrijving = value; }
+        }
+
+        public Person()
+        {
+        }
+
+    }
+}
---
+++ OtherBindingOpdracht/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using OtherBindingOpdracht.Classes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace OtherBindingOpdracht
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        private ObservableCollection<Person> _ocPersonCollection = new ObservableCollection<Person>();
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private Person _Person = new Person();
+
+        public Person MyPerson
+        {
+            get { return _Person; }
+            set { _Person = value; NotifyPropertyChanged(); }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Toevoegen_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(MyPerson.Title) && !String.IsNullOrWhiteSpace(MyPerson.Auteur) && !String.IsNullOrWhiteSpace(MyPerson.Aantal.ToString()) && !String.IsNullOrWhiteSpace(MyPerson.Beschrijving))
+            {
+                _ocPersonCollection.Add(MyPerson);
+                MyPerson = new Person();
+            }
+        }
+        private void Verwijder_Click(object sender, RoutedEventArgs e)
+        {
+            _ocPersonCollection.Remove((Person)((Button)sender).DataContext);
+        }
+    }
+}
